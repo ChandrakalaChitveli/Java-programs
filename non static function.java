e01977266a0db7475ae6e61ad3398f68fcd22431
ChandrakalaChitveli <chanduchitveli@gmail.com>
2025-06-06 14:39:53 +0530

    Add files via upload

---
+++ non static function.java
@@ -0,0 +1,17 @@
+
+// non-static function
+public class Main
+{
+	public void add()
+	{
+	    int a=8;
+	    int b=6;
+	    int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args)
+	{
+	    Main m=new Main();
+	   m.add();
+	}
+}
