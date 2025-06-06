cf54f53ad33db399e88c49081cea2b75f2f2d63f
ChandrakalaChitveli <chanduchitveli@gmail.com>
2025-06-06 14:55:03 +0530

    Add files via upload

---
+++ maxmum in array.java
@@ -0,0 +1,18 @@
+//maxmimum number
+import java.util.*;
+public class Main4{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int []a=new int[5];
+for(int i=0;i<a.length;i++)
+{
+a[i]=sc.nextInt();
+}int m=a[0];
+for(int i=0;i<a.length;i++)
+{
+if(a[i]>m)
+{
+    m=a[i];
+}}
+System.out.println(m);
+}}
